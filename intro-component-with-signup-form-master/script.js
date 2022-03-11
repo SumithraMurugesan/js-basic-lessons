24acf0c363aa24856f50ac5fcafc864eb1bcd49f
Sumithra M <sumi3006@global.chainsys.com>
2022-03-11 17:41:07 +0530

    modification

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,62 +1,39 @@
-function validation() {
-  if (FirstName()) {
-    if (LastName()) {
-      if (Email()) {
-        if (Password()) {
-            alert("yes")
-          return true
-        }
-      }
-    }
+
+function checkForm() {
+  var formFname = document.myForm.fname;
+  var formLname = document.myForm.lname;
+  var formEmail = document.myForm.email;
+  var formPassword = document.myForm.password;
+  if (formFname.value.indexOf(" ") == -1) {
+
+    document.querySelector(".error-msg-f").style.display = 'block';
+      formFname.focus();
+      return;
   }
-  return false
-}
+  else{
+    return true;
+    form.focus();
 
-function FirstName () {
-  const first = document.getElementsByName('fname')[0].value
-  const FirstName = first.length
-
-  if (FirstName == 0) {
-    document.querySelector('.error-msg-f').style.display = 'block'
-    document.querySelector('.error-f').style.visibility = 'visible'
-    return false
   }
-  return true
-}
+  if (formLname.value.indexOf(" ") == -1) {
+    document.querySelector(".error-msg-l").style.display = 'block';
 
-function LastName () {
-  const last = document.getElementsByName('lname')[0].value
-  const LastName = last.length
-
-  if (LastName == 0) {
-    document.querySelector('.error-msg-l').style.display = 'block'
-    document.querySelector('.error-l').style.visibility = 'visible'
-    return false
+      form.focus();
+      return;
   }
-  return true
-}
+  if (formEmail.value.indexOf("@") == -1 || 
+  formEmail.value.indexOf(".") == -1) {
+    document.querySelector(".error-msg-e").style.display = 'block';
 
-function Email () {
-  const email = document.getElementsByName('email')[0]
-  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-
-  if (email.value.match(mailformat)) {
-    return true
+      formEmail.focus();
+      return;
   }
-  document.querySelector('.error-msg-e').style.display = 'block'
-  document.querySelector('.error-e').style.visibility = 'visible'
-  return false
-}
 
-function Password () {
-  const pass = document.getElementsByName('password')[0].value
-  const password = pass.length
+  if (formPassword.value.indexOf(" ") == -1) {
+    document.querySelector(".error-msg-p").style.display = 'block';
 
-  if (password == 0) {
-      alert("enter the password");
-    document.querySelector('.error-msg-p').style.display = 'block'
-    document.querySelector('.error-p').style.visibility = 'visible'
-    return false
+    
+      formPassword.focus();
+      return;
   }
-  return true
 }
