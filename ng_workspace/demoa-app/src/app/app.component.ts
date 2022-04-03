730863ef577e0badc2d9ad68384275db70c465ae
Sumithra M <sumi3006@global.chainsys.com>
2022-04-03 23:53:42 +0530

    initial commit

--- ng_workspace/demoa-app/src/app/app.component.ts
+++ ng_workspace/demoa-app/src/app/app.component.ts
@@ -1,19 +1,67 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'my-root',//should in kebab style
+//   templateUrl: './app.component.html',
+//   // template: '<h1>Inline html content</h1>',
+//   styleUrls: ['./app.component.css']//optionl one
+// })
+// export class AppComponent {
+//   title = 'demoa-app';
+//   location = 'chennai';
+//   total: number = 1000;
+//   hits: number = 50;
+
+//   onBoxValueChange(paramData: any) {
+//     console.log("app component:" + paramData.value);
+//   }
+// }
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'my-root',//should in kebab style
+  selector: 'my-root',
   templateUrl: './app.component.html',
-  // template: '<h1>Inline html content</h1>',
-  styleUrls: ['./app.component.css']//optionl one
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoa-app';
-  location = 'chennai';
-  total: number = 1000;
-  hits: number = 50;
-
-  onBoxValueChange(paramData: any) {
-    console.log("app component:" + paramData.value);
+  fg = new FormGroup({
+    fname: new FormControl("", [Validators.required]), lname: new FormControl("", [Validators.required]), dob: new FormControl("", [Validators.required]), gender: new FormControl("", [Validators.required]),
+    city: new FormControl("", [Validators.required]), date: new FormControl("", [Validators.required]), time: new FormControl("", [Validators.required]), lab: new FormControl("", [Validators.required]), report: new FormControl("", [Validators.required])
+    , result: new FormControl("", [Validators.required]), aadhar: new FormControl("", [Validators.required])
+  });
+  store: any = [];
+  input: any;
+  headers = ["First Name", "Last Name", "Date of Birth", "Gender", "City", "Date Of Test", "Time Of Test", "Lab Name",
+    "Report Number", "Result", "Aadhar Number"];
+  title: any;
+  searchvalue: any = [];
+  add() {
+    if (this.fg.dirty === true) {
+      console.log(this.store.push(this.fg.value));
+      console.log(this.store);
+      this.fg.reset();
+      (<HTMLDivElement>document.getElementById("msg")).style.display = "none";
+    }
+    // else{
+    //   alert("*************Please, must fill the empty fields in the form!!!**********");
+    // }
+  }
+  search() {
+    console.log((<HTMLDivElement>document.getElementById("find")).innerHTML);
+    if ((<HTMLDivElement>document.getElementById("find")).innerText == "") {
+      (<HTMLDivElement>document.getElementById("msg")).innerText = "***********THERE IS NO RECORD FOUND AND FIELD WAS EMPTY!!!*********";
+    }
+    this.input = (<HTMLInputElement>document.getElementById("find")).value;
+    this.store.forEach((element: any) => {
+      if (element.aadhar == this.input) {
+        this.searchvalue.push(element);
+      }
+    });
+    (<HTMLInputElement>document.getElementById("find")).value = "";
+  }
+  removefield() {
+    (<HTMLTableRowElement>document.getElementById('tr')).style.display = "none";
   }
 }
 
--- ng_workspace/demoa-app/src/app/app.module.ts
+++ ng_workspace/demoa-app/src/app/app.module.ts
@@ -1,34 +1,57 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms'
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { AppHeaderComponent } from './app.header.component';
+// import { BoxaComponent } from './boxa/boxa.component';
+// import { CustomerFormComponent } from './customer-form/customer-form.component';
+// import { CountDisplayComponentComponent } from './count-display-component/count-display-component.component';
+// import { HitComponentComponent } from './hit-component/hit-component.component';
+// import { CounterAppComponent } from './counter-app/counter-app.component';
+// import { RtPcrComponent } from './rt-pcr/rt-pcr.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HomeComponent,
+//     AppHeaderComponent,
+//     BoxaComponent,
+//     CustomerFormComponent,
+//     CountDisplayComponentComponent,
+//     HitComponentComponent,
+//     CounterAppComponent,
+//     RtPcrComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppHeaderComponent, AppComponent, HomeComponent]
+// })
+// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AppHeaderComponent } from './app.header.component';
-import { BoxaComponent } from './boxa/boxa.component';
-import { CustomerFormComponent } from './customer-form/customer-form.component';
-import { CountDisplayComponentComponent } from './count-display-component/count-display-component.component';
-import { HitComponentComponent } from './hit-component/hit-component.component';
-import { CounterAppComponent } from './counter-app/counter-app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AppHeaderComponent,
-    BoxaComponent,
-    CustomerFormComponent,
-    CountDisplayComponentComponent,
-    HitComponentComponent,
-    CounterAppComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppHeaderComponent, AppComponent, HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
